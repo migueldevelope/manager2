dc77d344cc30bf4ce311abcc84098d49551324f4
Miguel Eduardo da silva <miguel.develope@gmail.com>
2018-09-04 09:23:58 -0300

    include importclient

---
+++ ImportClient/Views/ViewPerson.cs
@@ -0,0 +1,13 @@
+﻿namespace ImportClient.Views
+{
+  public class ViewPerson
+  {
+    public string IdPerson { get; set; }
+    public string Name { get; set; }
+    public string IdAccount { get; set; }
+    public string Token { get; set; }
+    public string Photo { get; set; }
+    public byte ChangePassword { get; set; }
+    public byte TypeUser { get; set; }
+  }
+}
