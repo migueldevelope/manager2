1f2fc93727d161b902381b380e011cf426b6f7f1
Miguel Eduardo da silva <miguel.develope@gmail.com>
2018-09-06 09:30:17 -0300

    ok

--- Manager.Services/Specific/ServiceMailModel.cs
+++ Manager.Services/Specific/ServiceMailModel.cs
@@ -87,7 +87,7 @@ namespace Manager.Services.Specific
         var model = new MailModel
         {
           Status = EnumStatus.Enabled,
-          Message = "<html><head><style>#customers {font-family: 'Segoe UI', Arial, Helvetica, sans-serif;border-collapse: collapse;width: 100%;}#customers td, #customers th{border: 1px solid #ddd;padding: 8px;}#customers tr:nth-child(even){background-color: #f2f2f2;}#customers th {padding-top: 12px;padding-bottom: 12px;text-align: left;background-color: #4598dc;color: white;}</style></head><body><table id='customers'><tr><td colspan='3' style='font-size: medium; font-weight: bold; text-align: center;'>Plano de Desenvolvimento Individual</td></tr><tr><td colspan='3'>Sr(a). <strong>{Person}</strong>, abaixo estão relacionados os planos de desenvolvimento dos seus funcionários que já venceram, ou que estão vencendo este mês, por favor acesse o sistema e atualize os registros.</tr><tr><th>Funcionário</th><th>Plano de Ação</th><th>Prazo</th></tr>{List}<tr align='center'><td colspan='3'><a href='{Link}'>Clique aqui para acessar o sistema</a></tr><tr align='center'><td colspan='3'>Obrigado por sua atenção.</tr></body></html>",
+          Message = "<html><head><style>#customers {font-family: 'Segoe UI', Arial, Helvetica, sans-serif;border-collapse: collapse;width: 100%;}#customers td, #customers th{border: 1px solid #ddd;padding: 8px;}#customers tr:nth-child(even){background-color: #f2f2f2;}#customers th {padding-top: 12px;padding-bottom: 12px;text-align: left;background-color: #4598dc;color: white;}</style></head><body><table id='customers'><tr><td colspan='3' style='font-size: medium; font-weight: bold; text-align: center;'>Plano de Desenvolvimento Individual</td></tr><tr><td colspan='3'>Sr(a). <strong>{Person}</strong>, abaixo estão relacionados os planos de desenvolvimento dos seus funcionários que já venceram, ou que estão vencendo este mês, por favor acesse o sistema e atualize os registros.</tr><tr><th>Funcionário</th><th>Plano de Ação</th><th>Prazo</th></tr>{List}<tr align='center'><td colspan='3'><a href='https://analisa.solutions/'>Clique aqui para acessar o sistema</a></tr><tr align='center'><td colspan='3'>Obrigado por sua atenção.</tr></body></html>",
           Subject = "Pendência do Plano de Desenvolvimento Individual (PDI)",
           Name = "developmentplan",
           Link = path
@@ -189,7 +189,7 @@ namespace Manager.Services.Specific
         var model = new MailModel
         {
           Status = EnumStatus.Enabled,
-          Message = "Ola <strong>{Person}</strong>,</br></br>O resultado do chekcpoint foi {Result}.</br></br>Para acessar o sistema <a href='{Link}'>clique aqui</a>.</br></br>Obrigado por sua atenção.",
+          Message = "Ola <strong>{Person}</strong>,</br></br>O resultado do chekcpoint foi {Result}.</br></br>Para acessar o sistema <a href='https://analisa.solutions/'>clique aqui</a>.</br></br>Obrigado por sua atenção.",
           Subject = "Restulado do chekcpoint",
           Name = "checkpointresult",
           Link = path
@@ -210,7 +210,7 @@ namespace Manager.Services.Specific
         var model = new MailModel
         {
           Status = EnumStatus.Enabled,
-          Message = "Ola <strong>{Person}</strong>,</br></br>É necessário que você acesse o sistema e realize uma aprovação do checkpointapproval.</br></br>Para acessar o sistema <a href='{Link}'>clique aqui</a>.</br></br>Obrigado por sua atenção.",
+          Message = "Ola <strong>{Person}</strong>,</br></br>É necessário que você acesse o sistema e realize uma aprovação do checkpointapproval.</br></br>Para acessar o sistema <a href='https://analisa.solutions/'>clique aqui</a>.</br></br>Obrigado por sua atenção.",
           Subject = "Aprovação de checkpointapproval",
           Name = "checkpointapproval",
           Link = path
@@ -231,7 +231,7 @@ namespace Manager.Services.Specific
         var model = new MailModel
         {
           Status = EnumStatus.Enabled,
-          Message = "Ola <strong>{Person}</strong>,</br></br>É necessário que você acesse o sistema e realize uma aprovação do Pdi.</br></br>Para acessar o sistema <a href='{Link}'>clique aqui</a>.</br></br>Obrigado por sua atenção.",
+          Message = "Ola <strong>{Person}</strong>,</br></br>É necessário que você acesse o sistema e realize uma aprovação do Pdi.</br></br>Para acessar o sistema <a href='https://analisa.solutions/'>clique aqui</a>.</br></br>Obrigado por sua atenção.",
           Subject = "Aprovação de PDI",
           Name = "planapproval",
           Link = path
@@ -252,7 +252,7 @@ namespace Manager.Services.Specific
         var model = new MailModel
         {
           Status = EnumStatus.Enabled,
-          Message = "Ola <strong>{Person}</strong>,</br></br>É necessário que você acesse o sistema e realize uma aprovação do OnBoarding.</br></br>Para acessar o sistema <a href='{Link}'>clique aqui</a>.</br></br>Obrigado por sua atenção.",
+          Message = "Ola <strong>{Person}</strong>,</br></br>É necessário que você acesse o sistema e realize uma aprovação do OnBoarding.</br></br>Para acessar o sistema <a href='https://analisa.solutions/'>clique aqui</a>.</br></br>Obrigado por sua atenção.",
           Subject = "Aprovação de OnBoarding",
           Name = "onboardingapproval",
           Link = path
@@ -273,7 +273,7 @@ namespace Manager.Services.Specific
         var model = new MailModel
         {
           Status = EnumStatus.Enabled,
-          Message = "Ola <strong>{Person}</strong>,</br></br>É necessário que você acesse o sistema e realize uma aprovação do Monitoring.</br></br>Para acessar o sistema <a href='{Link}'>clique aqui</a>.</br></br>Obrigado por sua atenção.",
+          Message = "Ola <strong>{Person}</strong>,</br></br>É necessário que você acesse o sistema e realize uma aprovação do Monitoring.</br></br>Para acessar o sistema <a href='https://analisa.solutions/'>clique aqui</a>.</br></br>Obrigado por sua atenção.",
           Subject = "Aprovação de Monitoring",
           Name = "monitoringapproval",
           Link = path
@@ -309,7 +309,7 @@ namespace Manager.Services.Specific
         var model = new MailModel
         {
           Status = EnumStatus.Enabled,
-          Message = "<html><head><style>#customers { font-family: 'Segoe UI', Arial, Helvetica, sans-serif; border-collapse: collapse; width: 100%; }#customers td, #customers th {border: 1px solid #ddd; padding: 8px; }#customers tr:nth-child(even){background-color: #f2f2f2;}#customers th{padding-top: 12px;padding-bottom: 12px; text-align: left; background-color: #4598dc; color: white;}</style></head><body><table id='customers'><tr><td colspan='2' style='font-size: medium; font-weight: bold; text-align: center;'>Acordos de Expectativas Pendentes</td></tr><tr><td colspan='2'>Sr(a). <strong>{Person}</strong>, abaixo estão relacionados os acordos de expectativas dos seus funcionários que ainda não tem registro de conclusão, por favor acesse o sistema e realize estes acordos. </tr><tr><th>Funcionário</th><th>Situação</th></tr>{List}<tr align='center'><td colspan='2'><a href='{Link}'>Clique aqui para acessar o sistema</a></tr><tr align='center'><td colspan='2'>Obrigado por sua atenção.</tr></body></html>",
+          Message = "<html><head><style>#customers { font-family: 'Segoe UI', Arial, Helvetica, sans-serif; border-collapse: collapse; width: 100%; }#customers td, #customers th {border: 1px solid #ddd; padding: 8px; }#customers tr:nth-child(even){background-color: #f2f2f2;}#customers th{padding-top: 12px;padding-bottom: 12px; text-align: left; background-color: #4598dc; color: white;}</style></head><body><table id='customers'><tr><td colspan='2' style='font-size: medium; font-weight: bold; text-align: center;'>Acordos de Expectativas Pendentes</td></tr><tr><td colspan='2'>Sr(a). <strong>{Person}</strong>, abaixo estão relacionados os acordos de expectativas dos seus funcionários que ainda não tem registro de conclusão, por favor acesse o sistema e realize estes acordos. </tr><tr><th>Funcionário</th><th>Situação</th></tr>{List}<tr align='center'><td colspan='2'><a href='https://analisa.solutions/'>Clique aqui para acessar o sistema</a></tr><tr align='center'><td colspan='2'>Obrigado por sua atenção.</tr></body></html>",
           Subject = "Pendência de Acordo de Expectativas",
           Name = "expectationspendingmanager",
           Link = path
@@ -345,7 +345,7 @@ namespace Manager.Services.Specific
         var model = new MailModel
         {
           Status = EnumStatus.Enabled,
-          Message = "<html><head><style>body{font-family: 'Segoe UI', Arial, Helvetica, sans-serif; border-collapse: collapse; width: 100%;}</style></head><body>Olá <strong>{Person}</strong>,<br /><br />Existe um acordo de expectativa pendende de sua aprovação, por favor, <a href='{Link}'>clique aqui</a> para acessar o sistema.<br /><br />Obrigado por sua atenção.</body></html>",
+          Message = "<html><head><style>body{font-family: 'Segoe UI', Arial, Helvetica, sans-serif; border-collapse: collapse; width: 100%;}</style></head><body>Olá <strong>{Person}</strong>,<br /><br />Existe um acordo de expectativa pendende de sua aprovação, por favor, <a href='https://analisa.solutions/'>clique aqui</a> para acessar o sistema.<br /><br />Obrigado por sua atenção.</body></html>",
           Subject = "Pendência de Acordo de Expectativas",
           Name = "expectationspendingemployee",
           Link = path
