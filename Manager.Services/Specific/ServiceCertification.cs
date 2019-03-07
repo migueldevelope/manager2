ee22647367fa8e785497aff4ade8b2210cfab1c5
Miguel Eduardo da silva <miguel.develope@gmail.com>
2019-03-07 11:42:49 -0300

    okk

--- Manager.Services/Specific/ServiceCertification.cs
+++ Manager.Services/Specific/ServiceCertification.cs
@@ -486,7 +486,7 @@ namespace Manager.Services.Specific
         {
           IdPerson = person._id,
           Name = person.Name,
-          Mail = person.Name,
+          Mail = person.Mail,
           Status = EnumStatus.Enabled,
           StatusCertificationPerson = EnumStatusCertificationPerson.Wait,
           Comments = null,
@@ -577,7 +577,15 @@ namespace Manager.Services.Specific
           certification.DateBegin = DateTime.Now;
           foreach (var item in certification.ListPersons)
           {
-            Mail(certification.Person, new BaseFields() { Name = item.Name, Mail = item.Mail });
+            try
+            {
+              Mail(certification.Person, new BaseFields() { Name = item.Name, Mail = item.Mail });
+            }
+            catch (Exception)
+            {
+
+            }
+            
           }
         }
 
