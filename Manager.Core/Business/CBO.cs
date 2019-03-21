44a0f1e102630cf87ad1ee958ef4c136d25be15d
Miguel Eduardo da silva <miguel.develope@gmail.com>
2019-03-21 09:27:38 -0300

    k

--- Manager.Core/Business/CBO.cs
+++ Manager.Core/Business/CBO.cs
@@ -5,7 +5,7 @@ namespace Manager.Core.Business
   /// <summary>
   /// Objeto persiste no banco de dados
   /// </summary>
-  public class CBO : BaseEntity
+  public class Cbo : BaseEntity
   {
     public string Name { get; set; }
     public string Code { get; set; }
