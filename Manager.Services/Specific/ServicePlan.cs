504a606057a52a7942606941e48530f9bb6c71ba
Miguel Eduardo da silva <miguel.develope@gmail.com>
2019-05-31 15:45:18 -0300

    plan status monitoring

--- Manager.Services/Specific/ServicePlan.cs
+++ Manager.Services/Specific/ServicePlan.cs
@@ -650,7 +650,7 @@ namespace Manager.Services.Specific
           else
             Task.Run(() => Mail(person, person.User.Name, person.User.Mail));
         }
-          
+
 
         servicePlan.Update(plan, null);
         //return "Plan altered!";
@@ -1073,37 +1073,39 @@ namespace Manager.Services.Specific
 
         foreach (var res in plan)
         {
-          result.Add(new ViewGetPlan()
-          {
-            _id = res._id,
-            Name = res.Name,
-            DateInclude = res.DateInclude,
-            Deadline = res.Deadline,
-            Description = res.Description,
-            Skills = res.Skills?.Select(p => new ViewListSkill()
+          var monitoring = serviceMonitoring.GetAllNewVersion(p => p._id == res._idMonitoring).Result.FirstOrDefault().StatusMonitoring;
+          if (monitoring == EnumStatusMonitoring.End)
+            result.Add(new ViewGetPlan()
             {
-              _id = p._id,
-              TypeSkill = p.TypeSkill,
-              Concept = p.Concept,
-              Name = p.Name
-            }).ToList(),
-            UserInclude = res.Person == null ? null : servicePerson.GetAll(p => p._id == res.Person._id).FirstOrDefault()?._id,
-            TypePlan = res.TypePlan,
-            _idPerson = res.Person._id,
-            NamePerson = res.Person.User.Name,
-            SourcePlan = res.SourcePlan,
-            IdMonitoring = res._id,
-            Evaluation = res.Evaluation,
-            StatusPlan = res.StatusPlan,
-            TypeAction = res.TypeAction,
-            StatusPlanApproved = res.StatusPlanApproved,
-            TextEnd = res.TextEnd,
-            TextEndManager = res.TextEndManager,
-            Status = res.Status,
-            DateEnd = res.DateEnd,
-            NewAction = res.NewAction,
-            Bomb = GetBomb((DateTime.Parse(res.Deadline.ToString()) - DateTime.Now).Days)
-          });
+              _id = res._id,
+              Name = res.Name,
+              DateInclude = res.DateInclude,
+              Deadline = res.Deadline,
+              Description = res.Description,
+              Skills = res.Skills?.Select(p => new ViewListSkill()
+              {
+                _id = p._id,
+                TypeSkill = p.TypeSkill,
+                Concept = p.Concept,
+                Name = p.Name
+              }).ToList(),
+              UserInclude = res.Person == null ? null : servicePerson.GetAll(p => p._id == res.Person._id).FirstOrDefault()?._id,
+              TypePlan = res.TypePlan,
+              _idPerson = res.Person._id,
+              NamePerson = res.Person.User.Name,
+              SourcePlan = res.SourcePlan,
+              IdMonitoring = res._id,
+              Evaluation = res.Evaluation,
+              StatusPlan = res.StatusPlan,
+              TypeAction = res.TypeAction,
+              StatusPlanApproved = res.StatusPlanApproved,
+              TextEnd = res.TextEnd,
+              TextEndManager = res.TextEndManager,
+              Status = res.Status,
+              DateEnd = res.DateEnd,
+              NewAction = res.NewAction,
+              Bomb = GetBomb((DateTime.Parse(res.Deadline.ToString()) - DateTime.Now).Days)
+            });
         }
 
 
