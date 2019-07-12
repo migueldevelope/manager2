6d5bde06bf12722147ea6bc7400b6ec18a9fbd7d
Miguel Eduardo da silva <miguel.develope@gmail.com>
2019-07-12 14:26:11 -0300

    ii

--- Manager.Views/BusinessList/ViewListPersonInfo.cs
+++ Manager.Views/BusinessList/ViewListPersonInfo.cs
@@ -11,7 +11,6 @@ namespace Manager.Views.BusinessList
   {
     public EnumTypeJourney TypeJourney { get; set; }
     public string Occupation { get; set; }
-    [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string _idManager { get; set; }
     public string Manager { get; set; }
