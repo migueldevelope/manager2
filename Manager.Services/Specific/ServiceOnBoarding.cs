e38e07dc2f692e69b419c8dcecd0dbbd6993f74a
Miguel Eduardo da silva <miguel.develope@gmail.com>
2019-07-24 17:46:18 -0300

    ok

--- Manager.Services/Specific/ServiceOnBoarding.cs
+++ Manager.Services/Specific/ServiceOnBoarding.cs
@@ -1624,7 +1624,7 @@ namespace Manager.Services.Specific
         {
 
         }
-        var body = model.Message.Replace("{Person}", person.User.Name).Replace("{Link}", model.Link).Replace("{Manager}", managername).Replace("{Company}", person.Company.Name).Replace("{Occupation}", person.Occupation.Name).Replace("{Company}", person.Company.Name).Replace("{Occupation}", person.Occupation.Name);
+        var body = model.Message.Replace("{Person}", person.User.Name).Replace("{Link}", model.Link).Replace("{Manager}", managername).Replace("{Company}", person.Company.Name).Replace("{Occupation}", person.Occupation.Name);
         var sendMail = new MailLog
         {
           From = new MailLogAddress("suporte@jmsoft.com.br", "Notificação do Analisa"),
@@ -1666,7 +1666,7 @@ namespace Manager.Services.Specific
 
         }
 
-        var body = model.Message.Replace("{Person}", person.User.Name).Replace("{Link}", model.Link).Replace("{Manager}", managername).Replace("{Company}", person.Company.Name).Replace("{Occupation}", person.Occupation.Name).Replace("{Company}", person.Company.Name).Replace("{Occupation}", person.Occupation.Name);
+        var body = model.Message.Replace("{Person}", person.User.Name).Replace("{Link}", model.Link).Replace("{Manager}", managername).Replace("{Company}", person.Company.Name).Replace("{Occupation}", person.Occupation.Name);
         var sendMail = new MailLog
         {
           From = new MailLogAddress("suporte@jmsoft.com.br", "Notificação do Analisa"),
@@ -1698,7 +1698,7 @@ namespace Manager.Services.Specific
         if (model.StatusMail == EnumStatus.Disabled)
           return;
 
-        var body = model.Message.Replace("{Person}", person.User.Name).Replace("{Link}", model.Link).Replace("{Manager}", person.Manager.Name).Replace("{Company}", person.Company.Name).Replace("{Occupation}", person.Occupation.Name).Replace("{Company}", person.Company.Name).Replace("{Occupation}", person.Occupation.Name);
+        var body = model.Message.Replace("{Person}", person.User.Name).Replace("{Link}", model.Link).Replace("{Manager}", person.Manager.Name).Replace("{Company}", person.Company.Name).Replace("{Occupation}", person.Occupation.Name);
         var sendMail = new MailLog
         {
           From = new MailLogAddress("suporte@jmsoft.com.br", "Notificação do Analisa"),
@@ -1740,7 +1740,7 @@ namespace Manager.Services.Specific
 
         }
 
-        var body = model.Message.Replace("{Person}", person.User.Name).Replace("{Link}", model.Link).Replace("{Manager}", managername).Replace("{Company}", person.Company.Name).Replace("{Occupation}", person.Occupation.Name).Replace("{Company}", person.Company.Name).Replace("{Occupation}", person.Occupation.Name);
+        var body = model.Message.Replace("{Person}", person.User.Name).Replace("{Link}", model.Link).Replace("{Manager}", managername).Replace("{Company}", person.Company.Name).Replace("{Occupation}", person.Occupation.Name);
         var sendMail = new MailLog
         {
           From = new MailLogAddress("suporte@jmsoft.com.br", "Notificação do Analisa"),
@@ -1782,7 +1782,7 @@ namespace Manager.Services.Specific
 
         }
 
-        var body = model.Message.Replace("{Person}", person.User.Name).Replace("{Link}", model.Link).Replace("{Manager}", managername).Replace("{Company}", person.Company.Name).Replace("{Occupation}", person.Occupation.Name).Replace("{Company}", person.Company.Name).Replace("{Occupation}", person.Occupation.Name);
+        var body = model.Message.Replace("{Person}", person.User.Name).Replace("{Link}", model.Link).Replace("{Manager}", managername).Replace("{Company}", person.Company.Name).Replace("{Occupation}", person.Occupation.Name);
         var sendMail = new MailLog
         {
           From = new MailLogAddress("suporte@jmsoft.com.br", "Notificação do Analisa"),
@@ -1823,7 +1823,7 @@ namespace Manager.Services.Specific
         {
 
         }
-        var body = model.Message.Replace("{Person}", person.User.Name).Replace("{Link}", model.Link).Replace("{Manager}", managername).Replace("{Company}", person.Company.Name).Replace("{Occupation}", person.Occupation.Name).Replace("{Company}", person.Company.Name).Replace("{Occupation}", person.Occupation.Name);
+        var body = model.Message.Replace("{Person}", person.User.Name).Replace("{Link}", model.Link).Replace("{Manager}", managername).Replace("{Company}", person.Company.Name).Replace("{Occupation}", person.Occupation.Name);
         var sendMail = new MailLog
         {
           From = new MailLogAddress("suporte@jmsoft.com.br", "Notificação do Analisa"),
--- Manager.Services/Specific/ServiceRecommendation.cs
+++ Manager.Services/Specific/ServiceRecommendation.cs
@@ -1,35 +1,52 @@
 ﻿using Manager.Core.Base;
 using Manager.Core.Business;
 using Manager.Core.Interfaces;
+using Manager.Core.Views;
 using Manager.Data;
+using Manager.Services.Auth;
 using Manager.Services.Commons;
 using Manager.Views.BusinessCrud;
 using Manager.Views.BusinessList;
 using Manager.Views.Enumns;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Manager.Services.Specific
 {
   public class ServiceRecommendation : Repository<Recommendation>, IServiceRecommendation
   {
+
+    private readonly ServiceAuthentication serviceAuthentication;
+    private readonly ServiceLog serviceLog;
     private readonly ServiceGeneric<Recommendation> serviceRecommendation;
     private readonly ServiceGeneric<RecommendationPerson> serviceRecommendationPerson;
     private readonly ServiceGeneric<Person> servicePerson;
     private readonly ServiceGeneric<SalaryScale> serviceSalaryScale;
+    private readonly ServiceGeneric<MailLog> serviceMail;
+    private readonly ServiceMailModel serviceMailModel;
+    private readonly IServiceControlQueue serviceControlQueue;
+
+    private string path;
 
     #region Constructor
-    public ServiceRecommendation(DataContext context) : base(context)
+    public ServiceRecommendation(DataContext context, DataContext contextLog, string pathToken, IServiceControlQueue _serviceControlQueue) : base(context)
     {
       try
       {
+        serviceAuthentication = new ServiceAuthentication(context, contextLog);
         serviceRecommendation = new ServiceGeneric<Recommendation>(context);
         serviceRecommendationPerson = new ServiceGeneric<RecommendationPerson>(context);
         servicePerson = new ServiceGeneric<Person>(context);
         serviceSalaryScale = new ServiceGeneric<SalaryScale>(context);
+        serviceMail = new ServiceGeneric<MailLog>(context);
+        serviceMailModel = new ServiceMailModel(context);
+        serviceLog = new ServiceLog(context, contextLog);
+        serviceControlQueue = _serviceControlQueue;
       }
       catch (Exception e)
       {
@@ -163,16 +180,33 @@ namespace Manager.Services.Specific
     {
       try
       {
-        Recommendation recommendation = serviceRecommendation.GetNewVersion(p => p._id == view.Recommendation._id).Result;
+        var verifyperson = serviceRecommendationPerson.GetNewVersion(p => p.Person._id == view.Person._id
+        & p.Recommendation._id == view.Recommendation._id & p._idColleague == _user._idUser).Result;
+        if (verifyperson != null)
+          return "already recommendation to person";
+
+        var verifycount = serviceRecommendationPerson.CountNewVersion(p => p._idColleague == _user._idUser
+        & p.Date.Value.Month == DateTime.Now.Month & p.Date.Value.Year == DateTime.Now.Year).Result;
+        if (verifycount == 3)
+          return "many recommendation month";
+
+        var person = servicePerson.GetNewVersion(p => p._id == view.Person._id).Result;
         RecommendationPerson recommendationperson = serviceRecommendationPerson.InsertNewVersion(
           new RecommendationPerson()
           {
             _id = view._id,
-            Recommendation = recommendation.GetViewList(),
+            Recommendation = view.Recommendation,
             Content = view.Content,
             Person = view.Person,
-            Comments = view.Comments
+            Comments = view.Comments,
+            _idColleague = _user._idUser,
+            Date = DateTime.Now
           }).Result;
+
+        Task.Run(() => Mail(person, view.Recommendation.Name));
+        Task.Run(() => SendQueue(recommendationperson._id, person._id));
+        Task.Run(() => LogSave(_user._idPerson, string.Format("Send reccomendation | {0}", recommendationperson._id)));
+
         return "RecommendationPerson added!";
       }
       catch (Exception e)
@@ -230,7 +264,7 @@ namespace Manager.Services.Specific
     {
       try
       {
-        total = servicePerson.CountNewVersion(p => p.TypeUser > EnumTypeUser.Administrator 
+        total = servicePerson.CountNewVersion(p => p.TypeUser > EnumTypeUser.Administrator
         & p.StatusUser != EnumStatusUser.Disabled & p.StatusUser != EnumStatusUser.ErrorIntegration
         & p.User.Name.ToUpper().Contains(filter.ToUpper())).Result;
         return servicePerson.GetAllNewVersion(p => p.TypeUser > EnumTypeUser.Administrator
@@ -246,5 +280,110 @@ namespace Manager.Services.Specific
 
     #endregion
 
+    #region private
+
+    private void Mail(Person person, string skill)
+    {
+      try
+      {
+        var model = serviceMailModel.Recommendation(path);
+        if (model.StatusMail == EnumStatus.Disabled)
+          return;
+
+        string managername = "";
+        try
+        {
+          managername = servicePerson.GetAllNewVersion(p => p.User._id == _user._idUser).Result.FirstOrDefault().User.Name;
+        }
+        catch (Exception)
+        {
+
+        }
+
+        var body = model.Message.Replace("{Person}", person.User.Name).Replace("{Link}", model.Link).Replace("{Manager}", managername).Replace("{Company}", person.Company.Name).Replace("{Occupation}", person.Occupation.Name).Replace("{Type}", skill);
+        var sendMail = new MailLog
+        {
+          From = new MailLogAddress("suporte@jmsoft.com.br", "Notificação do Analisa"),
+          To = new List<MailLogAddress>(){
+                        new MailLogAddress(person.User.Mail, person.User.Name)
+                    },
+          Priority = EnumPriorityMail.Low,
+          _idPerson = person._id,
+          NamePerson = person.User.Name,
+          Body = body,
+          StatusMail = EnumStatusMail.Sended,
+          Included = DateTime.Now,
+          Subject = model.Subject
+        };
+        var mailObj = serviceMail.InsertNewVersion(sendMail).Result;
+        var token = SendMail(path, person, mailObj._id.ToString());
+      }
+      catch (Exception e)
+      {
+        throw e;
+      }
+    }
+
+
+    private string SendMail(string link, Person person, string idmail)
+    {
+      try
+      {
+        string token = serviceAuthentication.AuthenticationMail(person);
+        using (var client = new HttpClient())
+        {
+          client.BaseAddress = new Uri(link.Substring(0, link.Length - 1) + ":5201/");
+          client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+          var resultMail = client.PostAsync("sendmail/" + idmail, null).Result;
+          return token;
+        }
+      }
+      catch (Exception e)
+      {
+        throw e;
+      }
+    }
+
+
+    private void SendQueue(string id, string idperson)
+    {
+      try
+      {
+        var data = new ViewCrudMaturityRegister
+        {
+          _idPerson = idperson,
+          TypeMaturity = EnumTypeMaturity.Recommendation,
+          _idRegister = id,
+          Date = DateTime.Now,
+          _idAccount = _user._idAccount
+        };
+
+        serviceControlQueue.SendMessageAsync(JsonConvert.SerializeObject(data));
+      }
+      catch (Exception e)
+      {
+        throw e;
+      }
+    }
+    private void LogSave(string iduser, string local)
+    {
+      try
+      {
+        var user = servicePerson.GetAllNewVersion(p => p._id == iduser).Result.FirstOrDefault();
+        var log = new ViewLog()
+        {
+          Description = "Access Recommendation",
+          Local = local,
+          _idPerson = user._id
+        };
+        serviceLog.NewLog(log);
+      }
+      catch (Exception e)
+      {
+        throw e;
+      }
+    }
+
+    #endregion
   }
 }
