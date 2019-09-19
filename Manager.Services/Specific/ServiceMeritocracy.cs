be9e406c16a29864a7304a83cf44044fd71499a9
Miguel Eduardo da silva <miguel.develope@gmail.com>
2019-09-19 11:01:58 -0300

    ok

--- Manager.Services/Specific/ServiceMeritocracy.cs
+++ Manager.Services/Specific/ServiceMeritocracy.cs
@@ -725,7 +725,8 @@ namespace Manager.Services.Specific
                 && p.StatusMeritocracy == EnumStatusMeritocracy.End).Result;
                 if (meritocracy.Count() > 0)
                 {
-                  var result = meritocracy.LastOrDefault().ResultEnd;
+                  var date = meritocracy.Max(p => p.DateEnd);
+                  var result = meritocracy.Where(p => p.DateEnd == date).LastOrDefault().ResultEnd;
                   list.Add(new ViewListMeritocracyResume()
                   {
                     Name = item.User?.Name,
@@ -805,7 +806,8 @@ namespace Manager.Services.Specific
                 && p.StatusMeritocracy == EnumStatusMeritocracy.End).Result;
                 if (meritocracy.Count() > 0)
                 {
-                  var result = meritocracy.LastOrDefault().ResultEnd;
+                  var date = meritocracy.Max(p => p.DateEnd);
+                  var result = meritocracy.Where(p => p.DateEnd == date).LastOrDefault().ResultEnd;
                   list.Add(new ViewListMeritocracyResume()
                   {
                     Name = item.User?.Name,
