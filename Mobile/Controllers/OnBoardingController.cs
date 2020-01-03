f40dc9ec7de1f4bfb0b305494d884f6d4661001a
Miguel Eduardo da silva <miguel.develope@gmail.com>
2020-01-03 09:01:27 -0300

    ok

--- Mobile/Controllers/OnBoardingController.cs
+++ Mobile/Controllers/OnBoardingController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
+using Manager.Core.Business;
 using Manager.Core.Interfaces;
+using Manager.Data;
+using Manager.Services.Commons;
 using Manager.Views.BusinessCrud;
 using Manager.Views.BusinessList;
 using Manager.Views.BusinessView;
@@ -10,6 +14,10 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Tools;
+using Tools.Data;
 
 namespace Mobile.Controllers
 {
@@ -20,7 +28,10 @@ namespace Mobile.Controllers
   [Route("onboarding")]
   public class OnBoardingController : DefaultController
   {
+    private readonly ServiceGeneric<Attachments> serviceAttachment;
     private readonly IServiceOnBoarding service;
+    private readonly DataContext context;
+    private readonly string blobKey;
 
     #region Constructor
     /// <summary>
@@ -30,6 +41,11 @@ namespace Mobile.Controllers
     /// <param name="contextAccessor">Token de segurança</param>
     public OnBoardingController(IServiceOnBoarding _service, IHttpContextAccessor contextAccessor) : base(contextAccessor)
     {
+      Config conn = XmlConnection.ReadVariablesSystem();
+      context = new DataContext(conn.Server, conn.DataBase);
+      blobKey = conn.BlobKey;
+      serviceAttachment = new ServiceGeneric<Attachments>(context);
+
       service = _service;
       service.SetUser(contextAccessor);
     }
@@ -338,6 +354,66 @@ namespace Mobile.Controllers
     public async Task<List<ViewExportStatusOnboardingGeral>> ExportStatusOnboarding([FromBody] List<ViewListIdIndicators> persons)
     {
       return await Task.Run(() => service.ExportStatusOnboarding(persons));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="idonboarding"></param>
+    /// <param name="iditem"></param>
+    /// <param name="typeuser"></param>
+    /// <returns></returns>
+    [Authorize]
+    [HttpPost("{idonboarding}/speech/{iditem}/{typeuser}/onboarding")]
+    public async Task<ObjectResult> PostSpeechRecognitionOnboarding(string idonboarding, string iditem, EnumUserComment typeuser)
+    {
+      foreach (var file in HttpContext.Request.Form.Files)
+      {
+        var ext = Path.GetExtension(file.FileName).ToLower();
+        if (ext == ".exe" || ext == ".msi" || ext == ".bat" || ext == ".jar")
+          return BadRequest("Bad file type.");
+      }
+      List<Attachments> listAttachments = new List<Attachments>();
+      var url = "";
+      foreach (var file in HttpContext.Request.Form.Files)
+      {
+        Attachments attachment = new Attachments()
+        {
+          Extension = Path.GetExtension(file.FileName).ToLower(),
+          LocalName = file.FileName,
+          Lenght = file.Length,
+          Status = EnumStatus.Enabled,
+          Saved = true
+        };
+        await this.serviceAttachment.InsertNewVersion(attachment);
+        try
+        {
+          CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(blobKey);
+          CloudBlobClient cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+          CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference(serviceAttachment._user._idAccount);
+          if (await cloudBlobContainer.CreateIfNotExistsAsync())
+          {
+            await cloudBlobContainer.SetPermissionsAsync(new BlobContainerPermissions
+            {
+              PublicAccess = BlobContainerPublicAccessType.Blob
+            });
+          }
+          CloudBlockBlob blockBlob = cloudBlobContainer.GetBlockBlobReference(string.Format("{0}{1}", attachment._id.ToString(), attachment.Extension));
+          blockBlob.Properties.ContentType = file.ContentType;
+          await blockBlob.UploadFromStreamAsync(file.OpenReadStream());
+          url = blockBlob.Uri.ToString();
+        }
+        catch (Exception)
+        {
+          attachment.Saved = false;
+          await serviceAttachment.Update(attachment, null);
+          throw;
+        }
+        var pathspeech = "http://10.0.0.16:5400/";
+        service.AddCommentsSpeech(idonboarding, iditem, url, typeuser, pathspeech);
+        listAttachments.Add(attachment);
+      }
+      return Ok(listAttachments);
     }
 
     #endregion
