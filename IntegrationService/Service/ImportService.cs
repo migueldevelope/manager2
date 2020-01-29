d6726fc169f35e47dd9ad0f69f18910c30750b28
Juremir Milani <juremir@jmsoft.com.br>
2020-01-29 16:12:35 -0300

    Ajsute de log

--- IntegrationService/Service/ImportService.cs
+++ IntegrationService/Service/ImportService.cs
@@ -1025,7 +1025,7 @@ namespace IntegrationService.Service
           foreach (ViewIntegrationMetadadosV1 colaborador in colaboradores)
           {
             ColaboradoresV2.Add(new ColaboradorV2Completo(colaborador, service.Param.CultureDate));
-            FileClass.SaveLog("juremir.txt", string.Format("{0};{1};{2};{3};{4};{5};{6};{7};{8};{9};{10};{11}", register, offset, colaborador.Cpf, colaborador.Empresa, colaborador.NomeEmpresa, colaborador.Estabelecimento, colaborador.NomeEstabelecimento, colaborador.Matricula, colaborador.Nome, colaborador.NomeCargo, colaborador.NomeClassContabil, colaborador.Email), EnumTypeLineOpportunityg.Register);
+            // FileClass.SaveLog("juremir.txt", string.Format("{0};{1};{2};{3};{4};{5};{6};{7};{8};{9};{10};{11}", register, offset, colaborador.Cpf, colaborador.Empresa, colaborador.NomeEmpresa, colaborador.Estabelecimento, colaborador.NomeEstabelecimento, colaborador.Matricula, colaborador.Nome, colaborador.NomeCargo, colaborador.NomeClassContabil, colaborador.Email), EnumTypeLineOpportunityg.Register);
             register++;
           }
           offset = offset + limit;
