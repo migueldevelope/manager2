92478027097e9ad55a08009aa2b84de8e4add23f
Miguel Eduardo da silva <miguel.develope@gmail.com>
2020-02-04 08:50:05 -0300

    ok

--- Manager.Services/Specific/ServiceEvent.cs
+++ Manager.Services/Specific/ServiceEvent.cs
@@ -1609,8 +1609,8 @@ namespace Manager.Services.Specific
               {
                 Course = course.GetViewList(),
                 Workload = item.Workload,
-                Begin = item.DateBegin.Value.AddDays(-1),
-                End = item.DateEnd.Value.AddDays(-1),
+                Begin = item.DateBegin.Value,
+                End = item.DateEnd.Value,
                 Name = item.NameEvent,
                 Entity = entity.GetCrudEntity(),
                 Event = new ViewListEvent()
