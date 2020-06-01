692a255b8d7f0dfea3c1b403b0d98dcb1d40ad6c
Miguel Eduardo da silva <miguel.develope@gmail.com>
2020-06-01 18:05:42 -0300

    ok

---
+++ Objective/Controllers/ObjectiveController.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Manager.Core.Interfaces;
+using Manager.Views.BusinessCrud;
+using Manager.Views.BusinessList;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Manager.Controllers
+{
+  /// <summary>
+  /// Controlador da empresa
+  /// </summary>
+  [Produces("application/json")]
+  [Route("objective")]
+  public class ObjectiveController
+  {
+    private readonly IServiceObjective service;
+
+    #region Constructor
+    /// <summary>
+    /// Construtor
+    /// </summary>
+    /// <param name="_service">Serviço da empresa</param>
+    /// <param name="contextAccessor">Token de segurança</param>
+    public ObjectiveController(IServiceObjective _service, IHttpContextAccessor contextAccessor) 
+    {
+      service = _service;
+      service.SetUser(contextAccessor);
+    }
+    #endregion
+
+    #region Objective
+    /// <summary>
+    /// Listar as empresas
+    /// </summary>
+    /// <param name="count">Quantidade de registros</param>
+    /// <param name="page">Página para mostrar</param>
+    /// <param name="filter">Filtro para o nome da empresa</param>
+    /// <returns>Lista de empresas cadastradas</returns>
+    [Authorize]
+    [HttpGet]
+    [Route("list")]
+    public async Task<List<ViewListObjective>> List(int count = 10, int page = 1, string filter = "")
+    {
+      long total = 0;
+      List<ViewListObjective> result = service.List(ref total, count, page, filter);
+      //Response.Headers.Add("x-total-count", total.ToString());
+      return await Task.Run(() => result);
+    }
+    /// <summary>
+    /// Cadastrar uma nova empresa
+    /// </summary>
+    /// <param name="view">Objeto de cadastro da empresa</param>
+    /// <returns></returns>
+    [HttpPost]
+    [Route("new")]
+    public async Task<ViewCrudObjective> Post([FromBody]ViewCrudObjective view)
+    {
+      return await Task.Run(() => service.New(view));
+    }
+    /// <summary>
+    /// Retorar a empresa para manutenção
+    /// </summary>
+    /// <param name="id">Identificador da empresa</param>
+    /// <returns>Objeto de manutenção da empresa</returns>
+    [Authorize]
+    [HttpGet]
+    [Route("get/{id}")]
+    public async Task<ViewCrudObjective> Get(string id)
+    {
+      return await Task.Run(() => service.Get(id));
+    }
+    /// <summary>
+    /// Alterar a empresa
+    /// </summary>
+    /// <param name="view">Objeto de manutenção da empresa</param>
+    /// <returns>Mensagem de sucesso</returns>
+    [Authorize]
+    [HttpPut]
+    [Route("update")]
+    public async Task<ViewCrudObjective> Update([FromBody]ViewCrudObjective view)
+    {
+      return await Task.Run(() => service.Update(view));
+    }
+    /// <summary>
+    /// Excluir uma empresa
+    /// </summary>
+    /// <param name="id">Identificação da empresa</param>
+    /// <returns>Mensagem de sucesso</returns>
+    [Authorize]
+    [HttpDelete]
+    [Route("delete/{id}")]
+    public async Task<string> Delete(string id)
+    {
+      return await Task.Run(() => service.Delete(id));
+    }
+    #endregion
+
+    #region KeyResult
+    /// <summary>
+    /// Listar estabelecimentos
+    /// </summary>
+    /// <param name="count">Quantidade de registros</param>
+    /// <param name="page">Página para mostrar</param>
+    /// <param name="filter">Filtro para o nome do estabelecimento</param>
+    /// <returns>Lista de estabelecimentos</returns>
+    [Authorize]
+    [HttpGet]
+    [Route("listkeyresult")]
+    public async Task<List<ViewListKeyResult>> ListKeyResult(int count = 10, int page = 1, string filter = "")
+    {
+      long total = 0;
+      List<ViewListKeyResult> result = service.ListKeyResult(ref total, count, page, filter);
+      //Response.Headers.Add("x-total-count", total.ToString());
+      return await Task.Run(() => result);
+    }
+    /// <summary>
+    /// Listar estabelecimento de uma empresa
+    /// </summary>
+    /// <param name="idobjective">Identificador da empresa</param>
+    /// <param name="count">Quantidade de registros</param>
+    /// <param name="page">Página para mostrar</param>
+    /// <param name="filter">Filtro para o nome do estabelecimento</param>
+    /// <returns>Lista de estabelecimentos da empresa</returns>
+    [Authorize]
+    [HttpGet]
+    [Route("listkeyresult/{idobjective}")]
+    public async Task<List<ViewListKeyResult>> ListKeyResult(string idobjective, int count = 10, int page = 1, string filter = "")
+    {
+      long total = 0;
+      List<ViewListKeyResult> result = service.ListKeyResult(idobjective, ref total, count, page, filter);
+      //Response.Headers.Add("x-total-count", total.ToString());
+      return await Task.Run(() => result);
+    }
+    /// <summary>
+    /// Novo estabelecimento
+    /// </summary>
+    /// <param name="view">Objeto de manutenção do estabelecimento</param>
+    /// <returns>Mensagem de sucesso</returns>
+    [HttpPost]
+    [Route("newkeyresult")]
+    public async Task<ViewCrudKeyResult> PostKeyResult([FromBody]ViewCrudKeyResult view)
+    {
+      return await Task.Run(() => service.NewKeyResult(view));
+    }
+    /// <summary>
+    /// Buscar estabelecimento para manutenção
+    /// </summary>
+    /// <param name="id">Identificador do estabelecimento</param>
+    /// <returns>Objeto de manutenção do estabelecimento</returns>
+    [Authorize]
+    [HttpGet]
+    [Route("getkeyresult/{id}")]
+    public async Task<ViewCrudKeyResult> ListKeyResult(string id)
+    {
+      return await Task.Run(() => service.GetKeyResult(id));
+    }
+    /// <summary>
+    /// Alterar o estabelecimento
+    /// </summary>
+    /// <param name="view">Objeto de manutenção do estabelecimento</param>
+    /// <returns>Mensagem de sucesso</returns>
+    [Authorize]
+    [HttpPut]
+    [Route("updatekeyresult")]
+    public async Task<ViewCrudKeyResult> UpdateKeyResult([FromBody]ViewCrudKeyResult view)
+    {
+      return await Task.Run(() => service.UpdateKeyResult(view));
+    }
+    /// <summary>
+    /// Deletar um estabelecimento
+    /// </summary>
+    /// <param name="id">Identificador do estabelecimento</param>
+    /// <returns>Mensagem de sucesso</returns>
+    [Authorize]
+    [HttpDelete]
+    [Route("deletekeyresult/{id}")]
+    public async Task<string> DeleteKeyResult(string id)
+    {
+      return await Task.Run(() => service.DeleteKeyResult(id));
+    }
+    #endregion
+
+  }
+}
---
+++ Objective/Startup.cs
@@ -0,0 +1,207 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Manager.Core.Interfaces;
+using Manager.Data;
+using Manager.Data.Infrastructure;
+using Manager.Services.Auth;
+using Manager.Services.Commons;
+using Manager.Services.Specific;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Swagger;
+using Tools;
+
+namespace Manager
+{
+  /// <summary>
+  /// Controle de inicialização da API
+  /// </summary>
+  public class Startup
+  {
+
+    /// <summary>
+    /// Construtor do controle
+    /// </summary>
+    /// <param name="configuration"></param>
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    /// <summary>
+    /// Propriedade de configuração
+    /// </summary>
+    public IConfiguration Configuration { get; }
+    private const string Secret = "db3OIsj+BXE9NZDy0t8W3TcNekrF+2d/1sFnWG4HnV8TZY30iTOdtVWJG8abWvB1GlOgJuQZdcF2Luqm/hccMw==";
+
+
+    /// <summary>
+    /// Registrador de serviços
+    /// </summary>
+    /// <param name="services">Coleção de serviços</param>
+    public void RegistreServices(IServiceCollection services)
+    {
+      DataContext _context;
+      var conn = XmlConnection.ReadVariablesSystem();
+      _context = new DataContext(conn.Server, conn.DataBase);
+
+
+      DataContext _contextLog;
+      _contextLog = new DataContext(conn.ServerLog, conn.DataBaseLog);
+
+      string serviceBusConnectionString = conn.ServiceBusConnectionString;
+
+      new MigrationHandle(_context._db).Migrate();
+
+
+      services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+      IServiceMaturity serviceMaturity = new ServiceMaturity(_context);
+      IServiceControlQueue serviceControlQueue = new ServiceControlQueue(serviceBusConnectionString, serviceMaturity);
+      
+      IServiceLog serviceLog = new ServiceLog(_context, _contextLog);
+      
+      IServiceParameters serviceParameters = new ServiceParameters(_context);
+      IServiceUser serviceUser = new ServiceUser(_context, _contextLog);
+      IServiceAuthentication serviceAuthentication = new ServiceAuthentication(_context, _contextLog, serviceControlQueue, conn.SignalRService);
+      IServiceObjective serviceObjective = new ServiceObjective(_context);
+
+      
+      services.AddSingleton(_ => serviceObjective);
+      services.AddSingleton(_ => serviceMaturity);
+      services.AddSingleton(_ => serviceControlQueue);
+      services.AddSingleton(_ => serviceUser);
+      services.AddSingleton(_ => serviceAuthentication);
+      services.AddSingleton(_ => serviceLog);
+      services.AddSingleton(_ => serviceParameters);
+
+
+    }
+
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    /// <summary>
+    /// Configurador de servicos
+    /// </summary>
+    /// <param name="services">Coleção de serviços</param>
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+      .AddJwtBearer(options =>
+      {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+          ValidateIssuer = false,
+          ValidateAudience = false,
+          ValidateLifetime = true,
+          ValidateIssuerSigningKey = true,
+          IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Secret))
+        };
+
+        options.Events = new JwtBearerEvents
+        {
+          OnAuthenticationFailed = context =>
+          {
+            Console.WriteLine("OnAuthenticationFailed: " + context.Exception.Message);
+            return Task.CompletedTask;
+          },
+          OnTokenValidated = context =>
+          {
+            Console.WriteLine("OnTokenValidated: " + context.SecurityToken);
+            return Task.CompletedTask;
+          }
+        };
+      });
+
+      services.AddCors(options =>
+      {
+        options.AddPolicy("CorsPolicy",
+                  builder => builder.WithOrigins("http://localhosts/*", "10.0.0.16", "https://test.analisa.solutions/*")
+                  //builder => builder.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader()
+                  .WithExposedHeaders("x-total-count")
+                  .AllowCredentials());
+      });
+
+      //services.AddMvc();
+      services.AddRazorPages();
+
+      services.AddControllersWithViews()
+      .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+      //services.AddSignalR();
+
+      // Configurando o serviço de documentação do Swagger
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1",
+            new Microsoft.OpenApi.Models.OpenApiInfo
+            {
+              Title = "Objective - Fluid",
+              Version = "v1",
+              Description = "Sistema de carreiras fluidas"
+            });
+        string caminhoAplicacao = PlatformServices.Default.Application.ApplicationBasePath;
+        string nomeAplicacao = PlatformServices.Default.Application.ApplicationName;
+        string caminhoXmlDoc = Path.Combine(caminhoAplicacao, $"{nomeAplicacao}.xml");
+        c.IncludeXmlComments(caminhoXmlDoc);
+      });
+
+      //      Configuration.GetConnectionString("10.0.0.16,port: 6379,password=bti9010");
+
+      //services.AddMemoryCache();
+      //services.AddDistributedRedisCache(options =>
+      //{
+      //  options.Configuration =
+      //      Configuration.GetConnectionString("redis://10.0.0.16:6379?password=bti9010");
+
+      //  options.InstanceName = "analisa";
+      //});
+
+      RegistreServices(services);
+
+    }
+
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    /// <summary>
+    /// Configuração de aplicação
+    /// </summary>
+    /// <param name="app">Aplicação</param>
+    /// <param name="env">Ambiente de hospedagem</param>
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      app.UseRouting();
+      app.UseCors(options => options.SetIsOriginAllowed(x => _ = true).AllowAnyMethod().AllowAnyHeader().AllowCredentials().WithExposedHeaders("x-total-count"));
+      app.UseAuthentication();
+      app.UseAuthorization();
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapDefaultControllerRoute();
+        endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+      });
+      //app.UseSignalR(routes =>
+      //{
+      //  routes.MapHub<MessagesHub>("/MessagesHub");
+      //});
+      // Ativando middlewares para uso do Swagger
+      app.UseSwagger();
+      app.UseSwaggerUI(c =>
+      {
+        c.RoutePrefix = "help";
+        //c.SwaggerEndpoint("..swagger/v1/swagger.json", "Manager");
+        c.SwaggerEndpoint("../swagger/v1/swagger.json", "Manager");
+      });
+    }
+  }
+}
