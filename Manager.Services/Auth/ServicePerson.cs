ae8be29e6d53f4f4fd80f6ee9d6d41ad4323f7a7
Miguel Eduardo da silva <miguel.develope@gmail.com>
2020-06-22 14:59:41 -0300

    ok

--- Manager.Services/Auth/ServicePerson.cs
+++ Manager.Services/Auth/ServicePerson.cs
@@ -1135,7 +1135,7 @@ namespace Manager.Services.Auth
         #region Ajustes na manutenção da pessoa
         if (person.StatusUser == EnumStatusUser.Disabled)
         {
-          person.TypeJourney = EnumTypeJourney.OutOfJourney;
+          //person.TypeJourney = EnumTypeJourney.OutOfJourney;
           var persons = servicePerson.CountNewVersion(p => p.User._id == person.User._id &&
           p.StatusUser != EnumStatusUser.Disabled).Result;
           if (persons == 0)
