f06ab02fe56f0efa6b1f38e286983473776d54fa
Miguel Eduardo da silva <miguel.develope@gmail.com>
2020-06-26 16:42:54 -0300

    ok

--- Manager.Views/BusinessView/ViewCertificate.cs
+++ Manager.Views/BusinessView/ViewCertificate.cs
@@ -10,8 +10,8 @@ namespace Manager.Views.BusinessView
     public string NameCourse { get; set; }
     public string NameEntity { get; set; }
     public string Content { get; set; }
-    public DateTime? DateBegin { get; set; }
-    public DateTime? DateEnd { get; set; }
+    public string DateBegin { get; set; }
+    public string DateEnd { get; set; }
     public decimal Workload { get; set; }
     public string Instructor { get; set; }
     public string NameParticipant { get; set; }
